8889dc125d4e385a80d9472a60987866973871ed
Jake Butler <47393028+jlb2637@users.noreply.github.com>
2025-07-10 07:37:33 -0400

    Add files via upload
    Updating Keymap.c now with most macros complete

--- QMK_Files/SnekPad12/default/keymap.c
+++ QMK_Files/SnekPad12/default/keymap.c
@@ -3,22 +3,159 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+		DCMUTE = SAFE_RANGE, //discord mute
+		DCDEFN, //discord deafen
+		DCJVC,  //discord join pre-selected voice channel, (currently games in chonk zone)
+		DCDC,   //discord disconnect from voice channel,
+		SPTPLY, //spotify play/pause
+		SPTYNX, //spotify next song
+		SPTYPV, //spotify next song
+		GITHUB, //opens my github page in chrome
+		CTLPNL, //open control panel
+		PCRNBW, //set PC RGB lights to standard pastel rainbow
+		PCRGBO, //turn off PC RGB lights
+		FLOPEN, //open a new fl studio project with my standard template
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┐
-     * │ 7 │ 8 │ 9 │
-     * ├───┼───┼───┤
-     * │ 4 │ 5 │ 6 │
-     * ├───┼───┼───┤
-     * │ 1 │ 2 │ 3 │
-     * ├───┼───┼───┤
-     * │ 0 │ . │ + │
-     * └───┴───┴───┘
+     * ┌────────┬────────┬────────┐
+     * │ DCMUTE │ DCDEFN │ DCJVC  │
+     * ├────────┼────────┼────────┤
+     * │ CTLPNL │ GITHUB │ DCDC   │
+     * ├────────┼────────┼────────┤
+     * │ PCRNBW │ PCRGBO │ FLOPEN │
+     * ├────────┼────────┼────────┤
+     * │ SPTYPV │ SPTPLY │ SPTYNX │
+     * └────────┴────────┴────────┘
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,   KC_PDOT, KC_PPLS
+        DCMUTE,   DCDEFN,  DCJVC,
+        PCRNBW,   PCRGBO,  DCDC,
+        CTLPNL,   GITHUB,  FLOPEN,
+        SPTYPV,   SPTPLY,  SPTYNX
     )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+	switch (keycode) {
+		case DCMUTE: //discord mute button! works even when discord is not focused
+			//dcmute macro events
+			if (record ->event.pressed) {
+			//events on press of key
+				//SEND_STRING(SS_LCTL(SS_LSFT("k")));
+				tap_code(KC_F13);
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case DCDEFN:
+			if (record ->event.pressed) {
+				tap_code(KC_F14);
+			} else {
+				
+			}
+		break;
+		
+		case DCJVC:
+			if (record ->event.pressed) {
+				tap_code(KC_F15);
+			} else {
+				
+			}
+		break;
+		
+		case DCDC:
+			if (record ->event.pressed) {
+				tap_code(KC_F16);
+			} else {
+				
+			}
+		break;
+		
+		case SPTPLY:
+			if (record ->event.pressed) {
+			//events on press of key
+				//SEND_STRING(SS_LCTL(SS_LSFT("k")));
+				tap_code(KC_MEDIA_PLAY_PAUSE);
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case SPTYPV:
+			if (record ->event.pressed) {
+			//events on press of key
+				tap_code(KC_MEDIA_PREV_TRACK);
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case SPTYNX:
+			if (record ->event.pressed) {
+			//events on press of key
+				tap_code(KC_MEDIA_NEXT_TRACK);
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case GITHUB:
+			if (record ->event.pressed) {
+			    //events on press of key
+			
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case CTLPNL:
+			if (record ->event.pressed) {
+			//events on press of key
+				SEND_STRING(SS_TAP(X_LGUI) "Control Panel" SS_DELAY(500) SS_TAP(X_ENT));
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case PCRNBW:
+			if (record ->event.pressed) {
+			    //events on press of key
+			
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case PCRGBO:
+			if (record ->event.pressed) {
+			    //events on press of key
+			
+			} else {
+				//events on release of key
+				
+			}
+		break;
+		
+		case FLOPEN:
+			if (record ->event.pressed) {
+			    //events on press of key
+				SEND_STRING(SS_TAP(X_LGUI) "fl studio" SS_DELAY(500) SS_TAP(X_ENT));
+			} else {
+				//events on release of key
+				
+			}
+		break;
+	}
+	return true;
 };
